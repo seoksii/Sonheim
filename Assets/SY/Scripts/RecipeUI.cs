4830ce91b82a8ffaa96bc37a2feda3cf438bdd25
Zif1519 <141081153+Zif1519@users.noreply.github.com>
2023-09-26 18:54:38 +0900

    [ADD] CombineUIPanel
    made UI_Panel for crafting system.

---
+++ Assets/SY/Scripts/RecipeUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CombineRecipe
+{
+    public ItemData_SY[] inputs;
+    public ItemData_SY output;
+}
+public class RecipeUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
