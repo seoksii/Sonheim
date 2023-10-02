4902c011f6db7a3db7efa4b1704005182391fd95
leesy015599 <leesy015599@gmail.com>
2023-10-02 15:10:12 +0900

    [ADD] Apply proper mesh collider

--- Assets/LSY/Scripts/ItemManager.cs
+++ Assets/LSY/Scripts/ItemManager.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
+    private static ItemManager _instance;
+
+    void Awake()
+    {
+        _instance = this;
+    }
+    
     public void DropNewItem(Vector3 position, ItemData itemData)
     {
         Instantiate(itemData.DropPrefab, position, Random.rotation);
     }
-
-    // public void DropFromInventory(Vector3 position, GameObject item)
-    // {
-    //     // position : 애니메이션이면 손 위치, 아니면 플레이어의 앞에
-    // }
 }
